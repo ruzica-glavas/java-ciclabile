3a38e751446ed5bf42bbe1aa9c3853f22ced1621
ruzica-glavas <ruzi99@hotmail.it>
2025-05-09 17:15:51 +0200

    add: constructor in ElencoInt

--- src/org/lessons/java/array/ElencoInt.java
+++ src/org/lessons/java/array/ElencoInt.java
@@ -1,5 +1,12 @@
 package org.lessons.java.array;
 
 public class ElencoInt {
+    int [] numeriInt;
 
+    //Costruttore
+
+    public ElencoInt(int [] numeriInt){
+        this.numeriInt=numeriInt;
+    }
+    
 }
